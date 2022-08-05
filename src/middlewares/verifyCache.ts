adfbad12b01bd3dc94f96918cf6ed15d37e451a7
Facundo Fierro <facundo.fierro@gmail.com>
2022-08-05 21:39:33 +0300

    Solution

--- src/middlewares/verifyCache.ts
+++ src/middlewares/verifyCache.ts
@@ -1,8 +1,9 @@
 import NodeCache from "node-cache";
+import{ Request, Response} from 'express';
 
 export const cache = new NodeCache({ stdTTL: 60 });
 
-export const verifyCache = (req:any, res:any, next:any) => {
+export const verifyCache = (req:Request, res:Response, next:any) => {
   try {
     if (cache.has('currencies')) {
       return res.status(200).json(cache.get('currencies'));
